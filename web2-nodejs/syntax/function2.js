dfe22b698c3d217474e0e7c88998a9f417ca2f15
minkyung_kwak <ellen10min@gmail.com>
2023-09-09 01:35:03 +0900

    Add: 25 - 함수

---
+++ web2-nodejs/syntax/function2.js
@@ -0,0 +1,17 @@
+/* console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+function sum(first, second) {
+  // parameter
+  console.log(first + second);
+}
+
+sum(2, 4); // argument */
+
+function sum(first, second) {
+  console.log("a");
+  return first + second;
+  console.log("b");
+}
+
+console.log(sum(2, 4));
