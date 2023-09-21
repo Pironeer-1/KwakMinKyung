300cfc4b929e92a406d14d46b0220cc848e4d003
minkyung_kwak <ellen10min@gmail.com>
2023-09-21 15:36:09 +0900

    Add: 14장

---  07React/chapter-07/src/index.js
+++  07React/chapter-07/src/index.js
@@ -9,11 +9,12 @@ import AttendanceBook from "./chapter_10/AttendanceBook";
 import Signup from "./chapter_11/Signup";
 import Calculator from "./chapter_12/Calculator";
 import ProfileCard from "./chapter_13/ProfileCard";
+import DarkOrLight from "./chapter_14/DarkOrLight";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>
 );
 
